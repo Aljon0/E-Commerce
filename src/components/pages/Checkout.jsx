5f9ecaae677c9768dde5921750878763a0306c97
Aljon0 <aljons702@gmail.com>
2025-07-19 22:10:23 +0800

    update import path for createCheckoutSession to use cartservice

--- src/components/pages/Checkout.jsx
+++ src/components/pages/Checkout.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { useCart } from "../../context/CartContext";
-import { createCheckoutSession } from "../../services/stripeService";
+import { createCheckoutSession } from "../../services/cartservice";
 import LoadingSpinner from "../common/LoadingSpinner";
 
 const Checkout = () => {
